8976433af9da3f47b2d3ad061b2fd159f93ba0ed
QuentinPetton <quentin.petton@hotmail.fr>
2024-10-30 17:00:40 +0100

    feat/ajout JoiSchema dans createTree & updateTree

--- src/controllers/tree.controller.js
+++ src/controllers/tree.controller.js
@@ -1,15 +1,16 @@
-import { Tree, TreeSpecies} from "../models/index.js";
-
+import Joi from "joi";
+import { Tree, TreeSpecies } from "../models/index.js";
 
 //todo limiter le nombre d'arbres envoyé ?
 export async function getAllTrees(req, res) {
-
-  try {    
+  try {
     const trees = await Tree.findAll({
-      include: [{
-        model: TreeSpecies,
-        as: "species",
-      }]
+      include: [
+        {
+          model: TreeSpecies,
+          as: "species",
+        },
+      ],
     });
     res.json(trees);
   } catch (error) {
@@ -23,88 +24,106 @@ export async function getOneTree(req, res) {
     const treeId = Number.parseInt(req.params.id);
     console.log(treeId);
 
-    if(Number.isNaN(treeId)){
+    if (Number.isNaN(treeId)) {
       res.status(400).send("L'id doit être un nombre");
       return;
     }
-    const tree = await Tree.findByPk(treeId, {  
-      include: [{
-        model: TreeSpecies,
-        as: "species",
-      }]
-  });
-    if(!tree){
+    const tree = await Tree.findByPk(treeId, {
+      include: [
+        {
+          model: TreeSpecies,
+          as: "species",
+        },
+      ],
+    });
+    if (!tree) {
       res.status(404).send("Arbre non trouvé");
       return;
     }
     res.json(tree);
-   
   } catch (error) {
     console.error(error);
     res.status(500).send("Une erreur s'est produite");
   }
 }
 
-export async function createTree(req, res) {  
-
+export async function createTree(req, res) {
   try {
-    //todo vérifier que l'id_species existe
-    //todo vérifier les données et les formater
-    // => JOI schéma
-    const {name, price_ht, quantity, age, id_species} = req.body;
-  
+    const { name, price_ht, quantity, age, id_species } = req.body;
+
+    //todo gérer le cas ou le champ name comprends ("toto11")
+
+    const createTreeSchema = Joi.object({
+      name: Joi.string().required(),
+      price_ht: Joi.number().precision(2).positive(),
+      quantity: Joi.number(),
+      age: Joi.number().required().positive(),
+      id_species: Joi.number(),
+    });
+    const { error } = createTreeSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.message });
+    }
+
     const createdTree = await Tree.create({
       name,
       price_ht,
       quantity,
       age,
-      id_species
+      id_species,
     });
     console.log(createTree);
-  
+
     res.status(201).json(createdTree);
-    
   } catch (error) {
     console.error(error);
     res.status(500).send("Une erreur s'est produite");
   }
 }
 
-export async function updateTree(req,res){
-
+export async function updateTree(req, res) {
   try {
     //récupérer l'id de l'arbre à modifier
     const treeId = Number.parseInt(req.params.id);
-  
-    if(Number.isNaN(treeId)){
+
+    if (Number.isNaN(treeId)) {
       res.status(400).send("L'id doit être un nombre");
       return;
     }
-  
+
     const tree = await Tree.findByPk(treeId);
-  
-    if(!tree){
+
+    if (!tree) {
       res.status(404).send("Arbre non trouvé");
       return;
     }
-  
-    const {name, price_ht, quantity, age, id_species} = req.body;
-  
+
+    const createTreeSchema = Joi.object({
+      name: Joi.string().required(),
+      price_ht: Joi.number().precision(2).positive(),
+      quantity: Joi.number(),
+      age: Joi.number().required().positive(),
+      id_species: Joi.number(),
+    });
+    const { error } = createTreeSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.message });
+    }
+
+    const { name, price_ht, quantity, age, id_species } = req.body;
+
     //on update les champs modifiés
     tree.name = name || tree.name;
     tree.price_ht = price_ht || tree.price_ht;
     tree.quantity = quantity || tree.quantity;
     tree.age = age || tree.age;
     tree.id_species = id_species || tree.id_species;
-  
+
     await tree.save();
-  
-  res.json(tree);
-    
+
+    res.json(tree);
   } catch (error) {
     console.error(error);
     res.status(500).send("Une erreur s'est produite");
   }
-
-
 }
