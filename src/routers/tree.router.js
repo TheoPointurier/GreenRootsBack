e271ecca540ecafc7916001c04c32d299d4d53e8
QuentinPetton <quentin.petton@hotmail.fr>
2024-10-30 11:26:42 +0100

    fix order seedingCampaign & CampaignLocation

--- src/routers/tree.router.js
+++ src/routers/tree.router.js
@@ -1,3 +1,9 @@
 import { Router } from 'express';
 
+import { treeController } from "../controllers/tree.controller.js";
+
 export const router = Router();
+
+
+// routes pour les arbres
+router.get('/trees', treeController.getAllTrees);
