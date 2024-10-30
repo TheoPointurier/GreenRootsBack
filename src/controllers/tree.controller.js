da64718c77c5130bcc75f6833dd64587da1e0225
QuentinPetton <quentin.petton@hotmail.fr>
2024-10-30 12:50:00 +0100

    feat route getAllTrees fonctionnelle. Fix: treeSpeciesModel

--- src/controllers/tree.controller.js
+++ src/controllers/tree.controller.js
@@ -1,6 +1,20 @@
-import { Tree } from "../models/tree.model.js";
+import { Tree, TreeSpecies} from "../models/index.js";
 
+
+//todo limiter le nombre d'arbres envoyé ?
 export async function getAllTrees(req, res) {
-  const trees = await Tree.findAll();
+  const trees = await Tree.findAll({
+    // association: "species"
+    include: [{
+      model: TreeSpecies,
+      as: "species",
+      attributes: [
+        "species_name", "description", "co2_absorption", "average_lifespan"
+      ]
+    }]
+
+  
+
+  });
   res.json(trees);
 }
