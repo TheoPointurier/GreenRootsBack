94ad0319782bd4aec9814bfe48dbcd4ca5a3d17c
TheoPointurier <theopointurier@gmail.com>
2024-10-30 14:55:00 +0100

    feat: modifier une campagne - arbres compris

--- src/controllers/campaign.controller.js
+++ src/controllers/campaign.controller.js
@@ -1,21 +1,20 @@
-import { Campaign, CampaignLocation, Tree } from '../models/index.js';
+import { Campaign, CampaignLocation, Tree } from "../models/index.js";
 
 export async function getAllCampaign(req, res) {
   try {
     const campaigns = await Campaign.findAll({
-      
       include: [
         {
           model: CampaignLocation,
-          as: 'location'
+          as: "location",
         },
         {
           model: Tree,
-          as: 'treesCampaign',
-        }
-      ]
+          as: "treesCampaign",
+        },
+      ],
     });
-    
+
     res.json(campaigns);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -29,18 +28,22 @@ export async function getCampaign(req, res) {
       include: [
         {
           model: CampaignLocation,
-          as: 'location'
+          as: "location",
         },
         // inclure l'assocition avec la table Tree
         {
           model: Tree,
-          as: 'treesCampaign',
-        }
-      ]
+          as: "treesCampaign",
+        },
+      ],
     });
-    
+
     if (campaign === null) {
-      res.status(404).json({ message: `La Campagne avec l'id ${req.params.id} n'a pas été trouvé` });
+      res
+        .status(404)
+        .json({
+          message: `La Campagne avec l'id ${req.params.id} n'a pas été trouvé`,
+        });
     } else {
       res.json(campaign);
     }
@@ -50,9 +53,10 @@ export async function getCampaign(req, res) {
 }
 
 export async function createCampaign(req, res) {
- try {
+  try {
     // Extraire les données de la requête
-    const { name, description, start_campaign, end_campaign, treesCampaign } = req.body;
+    const { name, description, start_campaign, end_campaign, treesCampaign } =
+      req.body;
 
     // Création de la campagne
     const campaign = await Campaign.create({
@@ -77,4 +81,49 @@ export async function createCampaign(req, res) {
       error: error.message,
     });
   }
-};
+}
+
+export async function updateCampaign(req, res) {
+  try {
+    const campaign = await Campaign.findByPk(req.params.id);
+
+    if (campaign === null) {
+      res
+        .status(404)
+        .json({
+          message: `La Campagne avec l'id ${req.params.id} n'a pas été trouvé`,
+        });
+    } else {
+      // Extraire les données de la requête
+      const { name, description, start_campaign, end_campaign, treesCampaign } =
+        req.body;
+
+      // Mettre à jour la campagne
+      await campaign.update({
+        name,
+        description,
+        start_campaign,
+        end_campaign,
+      });
+
+      // Gérer les associations d'arbres
+      if (treesCampaign && treesCampaign.length > 0) {
+        // `setTreesCampaign` va remplacer les associations existantes par celles spécifiées dans `treesCampaign`
+        await campaign.setTreesCampaign(treesCampaign);
+      } else {
+        // Si `treesCampaign` est vide ou non défini, on supprime toutes les associations d'arbres de cette campagne
+        await campaign.setTreesCampaign([]);
+      }
+
+      res.json({
+        message: "Campagne mise à jour avec succès et arbres associés",
+        campaign,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Erreur lors de la mise à jour de la campagne",
+      error: error.message,
+    });
+  }
+}
