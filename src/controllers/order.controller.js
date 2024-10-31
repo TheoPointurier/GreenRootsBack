7350370cb8edad6809de24fb55ab47f7a8838b01
QuentinPetton <quentin.petton@hotmail.fr>
2024-10-31 21:18:14 +0100

    feat updateOrder dans controller

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,4 +1,5 @@
-import { Order, OrderLine } from '../models/index.js';
+import Joi from 'joi';
+import { Campaign, Order, OrderLine, Tree, User } from '../models/index.js';
 
 export async function getAllOrders(req, res) {}
 
@@ -6,6 +7,57 @@ export async function getOneOrder(req, res) {}
 
 export async function createOrder(req, res) {}
 
-export async function updateOrder(req, res) {}
+export async function updateOrder(req, res) {
+  const orderId = Number.parseInt(req.params.id);
+
+  if (Number.isNaN(orderId)) {
+    res.status(400).send("L'id doit être un nombre");
+    return;
+  }
+  console.log(orderId);
+
+  const order = await Order.findByPk(orderId, {
+    include: [
+      {
+        model: User,
+        as: 'user',
+      },
+      {
+        model: OrderLine,
+        as: 'orderLines',
+        include: [
+          {
+            model: Tree,
+            as: 'tree',
+          },
+          {
+            model: Campaign,
+            as: 'campaign',
+          },
+        ],
+      },
+    ],
+  });
+
+  if (!order) {
+    res.status(404).send('Commande non trouvée');
+    return;
+  }
+
+  // On veux pouvoir éditer: les infos user, les infos orderLines
+  //todo le total_amout censé être number accepte les string, à corriger
+
+  //todo prévoir schéma Joi
+  const { total_amount, status, order_number } = req.body;
+
+  //on update les champs modifiés
+  order.total_amount = total_amount || order.total_amount;
+  order.status = status || order.status;
+  order.order_number = order_number || order.order_number;
+
+  await order.save();
+
+  res.json(order);
+}
 
 export async function deleteOrder(req, res) {}
--- src/routers/order.router.js
+++ src/routers/order.router.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import * as orderController from '../controllers/order.controller.js';
-import { router } from './tree.router.js';
 
-router.get('/orders', orderController.getAllOrders);
-router.get('/orders/:id', orderController.getOneOrder);
-router.post('/orders', orderController.createOrder);
+export const router = Router();
+
+// router.get('/orders', orderController.getAllOrders);
+// router.get('/orders/:id', orderController.getOneOrder);
+// router.post('/orders', orderController.createOrder);
 router.patch('/orders/:id', orderController.updateOrder);
-router.delete('/orders/:id', orderController.deleteOrder);
+// router.delete('/orders/:id', orderController.deleteOrder);
