1ad416f60aade81891efdb859d964059559f9d32
TheoPointurier <theopointurier@gmail.com>
2024-11-05 14:46:48 +0100

    feat cors ok - verifier message si front ou back

--- index.js
+++ index.js
@@ -7,17 +7,39 @@ import { router as apiRouter } from './src/routers/index.js';
 dotenv.config();
 const app = express();
 
-// Desactiver le header x-powered-by Express
+// Désactiver le header x-powered-by Express
 app.disable('x-powered-by');
 
-// CORS configuration
+const allowedOrigins = [process.env.CORS_ORIGIN]; // Remplace par l'origine autorisée
+
+// Middleware de vérification d'origine
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (!origin || allowedOrigins.includes(origin)) {
+    next(); // L'origine est autorisée, passer au middleware suivant
+  } else {
+    res.status(403).json({ message: 'Accès refusé' }); // Réponse générique sans information CORS
+  }
+});
+
+// Configuration CORS
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Accès refusé : origine non autorisée.'));
+    }
+  },
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // si tu utilises des cookies ou des tokens sur le frontend
 };
 
+// Ajout du CORS avec les paramètres définis
+app.use(cors(corsOptions));
+
+// Limitation de la fréquence des requêtes
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
@@ -25,21 +47,20 @@ const limiter = rateLimit({
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 });
 
-// Apply the rate limiting middleware to all requests.
+// Applique la limitation à toutes les requêtes
 app.use(limiter);
-
-// Ajout du CORS avec les paramètres définis
-app.use(cors(corsOptions));
 
 // Ajout du body parser
 app.use(express.urlencoded({ extended: false })); // Body parser pour les body des <form>
 app.use(express.json({ limit: '10kb' })); // Body parser pour les body de type "JSON"
 
+// Routes API
 app.use('/api', apiRouter);
 
+// Route racine
 app.use('/', (req, res) => {
   res.send("<h1>Bienvenue sur l'API de GreenRoots</h1>");
-  // Todo : Mettre la documentation de l'API à la place
+  // TODO : Mettre la documentation de l'API à la place
 });
 
 const PORT = process.env.PORT || 3000;
