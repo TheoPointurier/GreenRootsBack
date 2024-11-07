dd0fbb843c48a0cbba161c9abbe0fe2882c8cf03
TheoPointurier <theopointurier@gmail.com>
2024-11-07 11:21:21 +0100

    route get all campaigns backoffice ok

--- src/controllers/backOffice/campaign.controller.js
+++ src/controllers/backOffice/campaign.controller.js
@@ -1,4 +1,11 @@
 import {
+  Campaign,
+  CampaignLocation,
+  Country,
+  Tree,
+  TreeSpecies,
+} from '../../models/index.js';
+import {
   createCampaign,
   updateCampaign,
   deleteCampaign,
@@ -6,13 +13,49 @@ import {
   getCampaign,
 } from '../campaign.controller.js';
 
-//TOdo a faire, uniquement présent pour mettre dutexte dans le fichier
-const campaignController = {
-  createCampaign: createCampaign,
-  updateCampaign: updateCampaign,
-  deleteCampaign: deleteCampaign,
-  getAllCampaign: getAllCampaign,
-  getCampaign: getCampaign,
-};
+export async function getAllCampaignBackofice(req, res) {
+  try {
+    const campaigns = await Campaign.findAll({
+      include: [
+        {
+          model: CampaignLocation,
+          as: 'location',
+          include: [
+            {
+              model: Country,
+              as: 'country',
+            },
+          ],
+        },
+        {
+          model: Tree,
+          as: 'treesCampaign',
+          include: [
+            {
+              model: TreeSpecies,
+              as: 'species',
+            },
+          ],
+        },
+      ],
+    });
+    console.log(campaigns);
+    res.render('campaigns', { campaigns });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Une erreur s'est produite");
+  }
+}
 
-module.exports = campaignController;
+export function createCampaignBackofice(req, res) {
+  console.log('coucou');
+  createCampaign(req, res);
+
+  res.redirect('/');
+}
+
+export function updateCampaignBackofice(req, res) {
+  updateCampaign(req, res);
+
+  res.redirect('/');
+}
