047127660392da0c14697de74df87d68550a1929
TheoPointurier <theopointurier@gmail.com>
2024-11-07 17:32:30 +0100

    security ok

--- src/routers/backOffice/index.js
+++ src/routers/backOffice/index.js
@@ -5,14 +5,13 @@ import { router as campaignRouterBackOffice } from './campaign.router.js';
 import { router as userRouterBackOffice } from './user.router.js';
 import { router as authentificationRouterBackOffice } from './auth.router.js';
 import { router as orderRouterBackOffice } from './order.router.js';
-
+import verifyTokenAdmin from '../../middlewares/authentificationAdmin.js';
 // Router principal de l'API
 export const router = Router();
 
 // Brancher les routers secondaires :
-router.use(treeRouterBackOffice);
-router.use(campaignRouterBackOffice);
-router.use(userRouterBackOffice);
 router.use(authentificationRouterBackOffice);
-router.use(orderRouterBackOffice);
-router.use(authentificationRouterBackOffice);
+router.use(verifyTokenAdmin, treeRouterBackOffice);
+router.use(verifyTokenAdmin, campaignRouterBackOffice);
+router.use(verifyTokenAdmin, userRouterBackOffice);
+router.use(verifyTokenAdmin, orderRouterBackOffice);
