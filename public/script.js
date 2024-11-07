b1055184213fdb9c6d6557c1af2da22fb2266c26
QuentinPetton <quentin.petton@hotmail.fr>
2024-11-07 20:51:59 +0100

    ajout style modalEdit + amélioration parsing data

--- public/script.js
+++ public/script.js
@@ -8,17 +8,19 @@ function editCampaignModal(
   country,
   treeCampaign,
 ) {
-  treeCampaign = JSON.parse(treeCampaign);
-  console.log('treeCampaign:', treeCampaign);
+  const parsedTreeCampaign = JSON.parse(treeCampaign);
+
   document.getElementById('editId').value = id;
   document.getElementById('editName').value = name;
   document.getElementById('editDescription').value = description;
-  document.getElementById('editStartCampain').value = new Date(
-    start_campaign,
-  ).toLocaleDateString('fr-FR');
-  document.getElementById('editEndCampain').value = new Date(
-    end_campaign,
-  ).toLocaleDateString('fr-FR');
+
+  // Convertir la date au format YYYY-MM-DD pour les champs de type date
+  const startDate = new Date(start_campaign).toISOString().split('T')[0];
+  const endDate = new Date(end_campaign).toISOString().split('T')[0];
+
+  // Affecter les valeurs de date formatées
+  document.getElementById('editStartCampain').value = startDate;
+  document.getElementById('editEndCampain').value = endDate;
   document.getElementById('editLocation').value = location;
   document.getElementById('editCountry').value = country;
 
@@ -27,23 +29,23 @@ function editCampaignModal(
   const allRadioButtons = document.querySelectorAll('input[type="radio"]');
 
   // Mapper les IDs des arbres dans la campagne pour une vérification plus rapide
-  const treeIdsInCampaign = treeCampaign.map((tree) => tree.id);
+  const treeIdsInCampaign = parsedTreeCampaign.map((tree) => tree.id);
   console.log('treeIdsInCampaign:', treeIdsInCampaign);
 
   // Réinitialiser tous les boutons radio à "exclude" par défaut
-  allRadioButtons.forEach((radio) => {
+  for (const radio of allRadioButtons) {
     radio.checked = radio.value === 'exclude';
-  });
+  }
 
   // Cocher les boutons radio "include" pour les arbres dans `treeIdsInCampaign`
-  treeIdsInCampaign.forEach((treeId) => {
+  for (const treeId of treeIdsInCampaign) {
     const includeRadio = document.querySelector(
       `input[type="radio"][name="tree_${treeId}"][value="include"]`,
     );
     if (includeRadio) {
       includeRadio.checked = true;
     }
-  });
+  }
 
   // Afficher la modal
   document.getElementById('editModal').classList.remove('hidden');
