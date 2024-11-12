ee918d3792290a3bee731e92b8aaf5fc0b92f7b2
TheoPointurier <theopointurier@gmail.com>
2024-11-12 16:38:25 +0100

    crud user ok

--- public/usersScript.js
+++ public/usersScript.js
@@ -36,9 +36,6 @@ async function editUser(event, userId) {
   const country = document.querySelector(
     `#editUserModal-${userId} input[name="country"]`,
   ).value;
-  const entity_type = document.querySelector(
-    `#editUserModal-${userId} input[name="entity_type"]`,
-  ).value;
   const entity_name = document.querySelector(
     `#editUserModal-${userId} input[name="entity_name"]`,
   ).value;
@@ -48,7 +45,9 @@ async function editUser(event, userId) {
   const is_admin = document.querySelector(
     `#editUserModal-${userId} input[name="is_admin"]`,
   ).checked;
-  const id_role = 1; // Remplacez par la valeur appropriée ou récupérez-la du formulaire
+  const id_role = document.querySelector(
+    `#editUserModal-${userId} select[name="id_role"]`,
+  ).value;
 
   try {
     const response = await fetch(`/admin/users/${userId}`, {
@@ -66,7 +65,6 @@ async function editUser(event, userId) {
         city,
         postal_code,
         country,
-        entity_type,
         entity_name,
         entity_siret,
         is_admin,
@@ -98,4 +96,113 @@ function displayCreateUserModal() {
 
 function hideCreateUserModal() {
   document.getElementById('createUserModal').classList.add('hidden');
+}
+
+async function createUser(event) {
+  event.preventDefault();
+
+  const firstname = document.querySelector(
+    `#createUserModal input[name="firstname"]`,
+  ).value;
+  const lastname = document.querySelector(
+    `#createUserModal input[name="lastname"]`,
+  ).value;
+  const email = document.querySelector(
+    `#createUserModal input[name="email"]`,
+  ).value;
+  const password = document.querySelector(
+    `#createUserModal input[name="password"]`,
+  ).value;
+  const phone_number = document.querySelector(
+    `#createUserModal input[name="phone_number"]`,
+  ).value;
+  const street_number = document.querySelector(
+    `#createUserModal input[name="street_number"]`,
+  ).value;
+  const street = document.querySelector(
+    `#createUserModal input[name="street"]`,
+  ).value;
+  const city = document.querySelector(
+    `#createUserModal input[name="city"]`,
+  ).value;
+  const postal_code = document.querySelector(
+    `#createUserModal input[name="postal_code"]`,
+  ).value;
+  const country = document.querySelector(
+    `#createUserModal input[name="country"]`,
+  ).value;
+  const entity_name = document.querySelector(
+    `#createUserModal input[name="entity_name"]`,
+  ).value;
+  const entity_siret = document.querySelector(
+    `#createUserModal input[name="entity_siret"]`,
+  ).value;
+  const is_admin = document.querySelector(
+    `#createUserModal input[name="is_admin"]`,
+  ).checked;
+  const id_role = document.querySelector(
+    `#createUserModal select[name="id_role"]`,
+  ).value;
+
+  try {
+    const response = await fetch('/admin/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+        firstname,
+        lastname,
+        phone_number,
+        street_number,
+        street,
+        city,
+        postal_code,
+        country,
+        entity_name,
+        entity_siret,
+        is_admin,
+        id_role,
+      }),
+    });
+
+    if (response.ok) {
+      console.log('Utilisateur créé avec succès');
+      window.location.reload();
+    } else {
+      const errorMessage = await response.json();
+      console.error(
+        "Erreur lors de la création de l'utilisateur",
+        errorMessage,
+      );
+    }
+  } catch (error) {
+    console.error("Erreur réseau lors de la création de l'utilisateur", error);
+  }
+}
+
+async function deleteUser(userId) {
+  try {
+    const response = await fetch(`/admin/users/${userId}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      console.log('Utilisateur supprimé avec succès');
+      window.location.reload();
+    } else {
+      const errorMessage = await response.json();
+      console.error(
+        "Erreur lors de la suppression de l'utilisateur",
+        errorMessage,
+      );
+    }
+  } catch (error) {
+    console.error(
+      "Erreur réseau lors de la suppression de l'utilisateur",
+      error,
+    );
+  }
 }
