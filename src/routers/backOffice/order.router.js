70f478cdf47e97b03dd082eb94c68a869653f285
QuentinPetton <quentin.petton@hotmail.fr>
2024-11-13 16:47:03 +0100

    refacto orderControllerBackOffice

--- src/routers/backOffice/order.router.js
+++ src/routers/backOffice/order.router.js
@@ -4,11 +4,6 @@ import * as orderControllerBackOffice from '../../controllers/backOffice/order.c
 
 export const router = Router();
 
-//Route pour le back office
-
 router.get('/orders', orderControllerBackOffice.getAllOrdersBackOffice);
 router.patch('/orders/:id', orderControllerBackOffice.updateOrderBackOffice);
 router.delete('/orders/:id', orderControllerBackOffice.deleteOrderBackOffice);
-
-//todo voir si dispo pour user ou admin
-// router.get('/orders/:id', orderController.getOneOrder);
