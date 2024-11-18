7bd0f908e2d18afc8ffa5890ed554bebf16edaf2
Onitsukill <onitsukii@gmail.com>
2024-11-18 17:22:35 +0100

    modif regex mdp

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -80,7 +80,7 @@ export async function register(req, res) {
       password: Joi.string()
         .min(12)
         .pattern(
-          /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{12,}/,
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\p{L}\p{N}\p{P}\p{S}])[\p{L}\p{N}\p{P}\p{S}]{12,}$/u
         )
         .required(),
       firstname: Joi.string().required(),
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,7 +11,7 @@ const userSchema = Joi.object({
   password: Joi.string()
     .min(12)
     .pattern(
-      /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*À-ÿ])[A-Za-z\d!@#$%^&*À-ÿ]{12,}/,
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\p{L}\p{N}\p{P}\p{S}])[\p{L}\p{N}\p{P}\p{S}]{12,}$/u
     )
     .required(),
   firstname: Joi.string().required(),
