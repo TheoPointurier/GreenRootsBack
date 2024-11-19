ce74a2d313dd7ed01df64104f1be77f4fcc53551
TheoPointurier <theopointurier@gmail.com>
2024-11-19 09:39:44 +0100

    fix pour deployemennt avec ajout de helmet

--- src/controllers/backOffice/user.controller.js
+++ src/controllers/backOffice/user.controller.js
@@ -7,7 +7,7 @@ const createUserSchema = Joi.object({
   password: Joi.string()
     .min(12)
     .pattern(
-      /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*À-ÿ])[A-Za-z\d!@#$%^&*À-ÿ]{12,}/,
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\p{L}\p{N}\p{P}\p{S}])[\p{L}\p{N}\p{P}\p{S}]{12,}$/u,
     )
     .required(),
   firstname: Joi.string().required(),
@@ -29,7 +29,7 @@ const updateUserSchema = Joi.object({
   password: Joi.string()
     .min(12)
     .pattern(
-      /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*À-ÿ])[A-Za-z\d!@#$%^&*À-ÿ]{12,}/,
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\p{L}\p{N}\p{P}\p{S}])[\p{L}\p{N}\p{P}\p{S}]{12,}$/u,
     )
     .optional(),
   firstname: Joi.string().optional(),
